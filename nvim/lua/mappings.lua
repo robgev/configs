148e5214abde0a3f910bb089b63a3ad4097e2bb0
Robert Gevorgyan <robert1999.g@gmail.com>
2024-11-05 21:01:51 +0400

    Add a shortcut for diag floater

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -7,6 +7,7 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("n", "|", "<cmd>vsplit<cr>", { desc = "Vertical Split"})
 map("n", "\\", "<cmd>split<cr>", { desc = "Horizontal Split"})
+map("n", "<leader>f", function() vim.diagnostic.open_float() end, { desc = "Open Float Diag"})
 map("v", ".", ":norm.<CR>", { desc = "Make visual mode . work as normal mode's ."})
 map("i", "jk", "<ESC>")
 
